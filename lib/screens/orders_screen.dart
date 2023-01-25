82b91b258029a01b6852ec231613a9bfb9138207
Choupo Duval <choupoduval@gmail.com>
2023-01-25 02:17:22 +0100

    init

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -7,43 +7,55 @@ import '../widget/order_item.dart';
 
 class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
-  const OrdersScreen({super.key});
 
   @override
   State<OrdersScreen> createState() => _OrdersScreenState();
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-var _isLoading = false;
 
-@override
+Future? _ordersFuture;
+
+Future _obtainedFuture(){
+  return Provider.of<Orders>(context,listen: false).fetchAndSetOrders();
+}
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    Future.delayed(Duration.zero).then((_) async{
-      setState(() {
-        _isLoading = true;
-      });
-    await  Provider.of<Orders>(context,listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading=false;
-      });
-
-    });
+    _ordersFuture = _obtainedFuture();
   }
+
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    // final orderData = Provider.of<Orders>(context);
     return Scaffold(
       drawer: AppDrawer(),
       appBar: AppBar(title: Text('Your Orders'),),
-      body:_isLoading ? Center(child: CircularProgressIndicator()):  ListView.builder(
-        itemCount:orderData.orders.length ,
-        itemBuilder:(ctx,index){
-          return OrderItem(orderData.orders[index]);
+      body: FutureBuilder(future: _ordersFuture,
+        builder: (ctx,snapshot){
+       if(snapshot.connectionState == ConnectionState.waiting ){
+       return Center(child: CircularProgressIndicator());
+       }
+       else{
+        if(snapshot.hasError){
+          // handle error
+          return Center(child: Text('An Error Ocurred!'),);
+        }
+        else{
+       return Consumer<Orders>(
+         builder: (ctx, orderData, _) =>
+         ListView.builder(
+          itemCount:orderData.orders.length ,
+          itemBuilder:(ctx,index){
+            return OrderItem(orderData.orders[index]);
+       
+          } ),);
+        }
+       }
 
-        } ),
+      })
     );
   }
 }
